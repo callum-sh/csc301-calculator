b074e77475f677b47265d16f082daa663e60c55a
Josh Puglielli <joshpuglielli@pop-os.localdomain>
2022-10-13 23:20:48 -0400

    fuck you sharrock

--- core/models.py
+++ core/models.py
@@ -3,11 +3,7 @@ from django.db import models
 # Create your models here.
 class Items(models.Model):
     name = models.CharField(max_length=60)
-    price = models.FloatField()
+    price = models.DecimalField(decimal_places=2, max_digits=20, default=0.00)
 
     def __str__(self):
         return self.name
-    
-    def save(self, *args, **kwargs):
-        self.price = round(self.price, 2)
-        super(Items, self).save(*args, **kwargs)
