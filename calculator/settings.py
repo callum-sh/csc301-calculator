97d38f8112f120e6966c6f8d242a96d3a7840dbd
Callum Sharrock <38599348+callum-sh@users.noreply.github.com>
2022-10-14 15:14:52 -0400

    moving heroku settings to the bottom

--- calculator/settings.py
+++ calculator/settings.py
@@ -13,10 +13,6 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 import os
 from pathlib import Path
 
-if os.environ.get('GITHUB_ACTIONS') != 'true': 
-    import django_heroku
-    django_heroku.settings(locals()) 
-
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -132,3 +128,7 @@ STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+if os.environ.get('GITHUB_ACTIONS') != 'true': 
+    import django_heroku
+    django_heroku.settings(locals()) 
