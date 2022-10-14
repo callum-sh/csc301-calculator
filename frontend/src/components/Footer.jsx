947ce67a703e0beb935063b415603f551b3df016
Callum Sharrock <38599348+callum-sh@users.noreply.github.com>
2022-10-14 11:56:01 -0400

    fixing getSum dependency

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -5,34 +5,36 @@ import { useEffect } from "react";
 function Footer() {
 
     useEffect(() => {
+        const [Sum, setSum] = useState()
+        const [Total, setTotal] = useState()
+        const [Tax, setTax] = useState()
+
+        function getSum() {
+            axios({
+                method: "GET",
+                url: "/sum/",
+            }).then((response) => {
+                setSum(response.data["sum"]);
+                setTax(response.data["tax"]);
+                setTotal(response.data["total"]);
+                this.setState({
+                    sum: Sum,
+                    tax: Tax,
+                    total: Total
+                })
+            }).catch((error) => {
+                if (error.response) {
+                    console.log(error.response);
+                    console.log(error.response.status);
+                    console.log(error.response.headers);
+                }
+            })
+        }
+
         getSum()
     }, [])
 
-    const [Sum, setSum] = useState()
-    const [Total, setTotal] = useState()
-    const [Tax, setTax] = useState()
 
-    function getSum() {
-        axios({
-            method: "GET",
-            url: "/sum/",
-        }).then((response) => {
-            setSum(response.data["sum"]);
-            setTax(response.data["tax"]);
-            setTotal(response.data["total"]);
-            this.setState({
-                sum: Sum,
-                tax: Tax,
-                total: Total 
-            })
-        }).catch((error) => {
-            if (error.response) {
-                console.log(error.response);
-                console.log(error.response.status);
-                console.log(error.response.headers);
-            }
-        })
-    }
 
     return (
         <footer>
